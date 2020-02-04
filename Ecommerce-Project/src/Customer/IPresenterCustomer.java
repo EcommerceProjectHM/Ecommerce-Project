a7b93ea5860336bcda208107a8998143bcfe04c2
EcommerceProjectHM <bhuvanesh998@gmail.com>
2020-02-04 16:57:04 +0530

    Customer

---
+++ Ecommerce-Project/src/Customer/IPresenterCustomer.java
@@ -0,0 +1,11 @@
+package Customer;
+
+import java.sql.SQLException;
+
+public interface IPresenterCustomer {
+	String buy(int product_Id,int quantity,int price) throws SQLException;
+	public String addcart(int product_id, String customer) throws SQLException;
+	Object showcart(String customer) throws SQLException;
+	 public String removecart(int S_No) throws SQLException;
+}
+
---
+++ Ecommerce-Project/src/Customer/IViewCustomer.java
@@ -0,0 +1,11 @@
+package Customer;
+
+import java.sql.SQLException;
+
+public interface IViewCustomer {
+	void setPresenter(IPresenterCustomer presenetr) throws SQLException;
+	void buy(int product_Id) throws SQLException;
+	public void addToCart(int product_id) throws SQLException;
+	public void cartdetails() throws SQLException;
+}
+
