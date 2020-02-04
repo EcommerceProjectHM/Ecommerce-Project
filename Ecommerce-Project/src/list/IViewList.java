007c5477bb726dcff25c6e4804d28a111f6c5dfe
Subash9398 <subash9398@gmail.com>
2020-02-04 14:28:36 +0530

    Added list

---
+++ Ecommerce-Project/src/list/IViewList.java
@@ -0,0 +1,7 @@
+package list;
+
+import java.sql.SQLException;
+public interface IViewList
+{
+void setPresenter(IPresenterList prese) throws SQLException;
+}
---
+++ Ecommerce-Project/src/list/PresenterList.java
@@ -0,0 +1,22 @@
+package list;
+
+import java.sql.SQLException;
+public class PresenterList implements IPresenterList 
+{
+	private IModelList modellist;
+	private IViewList viewlist;
+
+	public PresenterList(IViewList view, IModelList model) 
+	{
+		this.modellist=model;
+		this.viewlist=view;
+	}
+	public Object category() throws SQLException
+	{ 
+		return modellist.category();
+	}
+	public Object categoryS_No(String i) throws SQLException
+	{
+		return modellist.productlist(i);
+	}
+}
